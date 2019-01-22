782e465436f9f6678f6ee55c8bd2eb75ffcc4248
Brian Illes <brianrilles@gmail.com>
2019-01-22 07:23:14 -0800

    mvp complete

--- assignments/objects.js
+++ assignments/objects.js
@@ -66,6 +66,7 @@ console.log(keven.email);
 console.log(gannie.name);
 // Antonietta's Gender
 console.log(antonietta.gender);
+
 // ==== Challenge 3: Object Methods ====
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
@@ -73,11 +74,14 @@ kennan.speak = function() {
   return `Hello, my name is ${this.name}!`;
 };
 console.log(kennan.speak());
+
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
 antonietta.multiplyNums = function(num1, num2) {
   return num1 * num2;
 };
+console.log(antonietta.multiplyNums(3, 4));
+
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
 // ==== Stretch Challenge: Nested Objects and the this keyword ====
